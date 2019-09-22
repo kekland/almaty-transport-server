092c846546a41478f4c3135068fe46630a072933
kekland <kk.erzhan@gmail.com>
2019-09-22 16:48:06 +0600

    Starting to work

---
+++ src/utils/position.ts
@@ -0,0 +1,24 @@
+export interface IPosition {
+  lat: number;
+  lon: number;
+}
+
+export class Position {
+  lat: number;
+  lon: number;
+
+  constructor(data: IPosition) {
+    if(data == null) {
+      throw Error('Provided data is null.')
+    }
+    this.lat = data.lat
+    this.lon = data.lon
+  }
+
+  static getDistanceBetweenPositions(a: Position, b: Position): number {
+    // TODO
+    return 0.0;
+  }
+
+  
+}
